9ad8d9425b5df131cfa5e5db6d639dd11f40e8a2
Brad <Graywf@icloud.com>
2014-01-27 22:21:14 -0500

    Python
    example for learn Python the hard way

---
+++ ex1.py
@@ -0,0 +1,7 @@
+print "Hello World!"
+print "Hello Again"
+print "I Like typing this."
+print "This is Fun."
+print 'Yay! Printing.'
+print "I'd much rather you not"
+print 'I "said" do not touch this.'
